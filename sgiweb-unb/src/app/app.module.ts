2148592139d241a02898b4ed71ca725d13cc2929
Elaine Cristina Meirelles <ecmeirelles@hotmail.com>
2017-09-11 08:29:15 -0300

    Adding material design dependencies

--- sgiweb-unb/src/app/app.module.ts
+++ sgiweb-unb/src/app/app.module.ts
@@ -3,6 +3,9 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
+import {MdButtonModule, MdInputModule} from '@angular/material';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+
 import { AppComponent } from './app.component';
 import { routing } from './app.routing';
 import { LoginComponent } from './login/login.component';
@@ -17,7 +20,10 @@ import { LoginService } from './login/shared/login.service';
     BrowserModule,
     FormsModule,
     HttpModule,
-    routing
+    routing,
+    BrowserAnimationsModule,
+    MdButtonModule,
+    MdInputModule
   ],
   providers: [LoginService],
   bootstrap: [AppComponent]
