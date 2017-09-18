6b0817b711c9a6b7f30de46e521e3c096e6ce588
Elaine Cristina Meirelles <ecmeirelles@hotmail.com>
2017-09-18 12:04:20 -0300

    Including comments and identation to code

--- sgiweb-unb/src/app/login/login.component.ts
+++ sgiweb-unb/src/app/login/login.component.ts
@@ -15,8 +15,8 @@ export class LoginComponent implements OnInit {
   ngOnInit() { }
   login: Login = new Login();
 
-  public isValidCPF = true;
-  public isValidCNPJ = true;
+  private isValidCPF = true;
+  private isValidCNPJ = true;
 
   public cpfMask = [/\d/, /\d/, /\d/, '.', /\d/, /\d/, /\d/, '.', /\d/, /\d/, /\d/, '-', /\d/, /\d/]
   public cnpjMask = [/\d/, /\d/, '.', /\d/, /\d/, /\d/, '.', /\d/, /\d/, /\d/, '/', /\d/, /\d/, /\d/, /\d/, '-', /\d/, /\d/]
@@ -36,117 +36,114 @@ export class LoginComponent implements OnInit {
     return !this.isValidCNPJ;
   }
 
-  validateCPF(cpf) {
-    this.isValidCPF = true;
-
-    if(cpf !== null) {
-      cpf = cpf.replace(/[^\d]+/g,'');
-
-      if(cpf.length === 11) {
-        let add = 0;
-        let rev = 0;
-
-        // Elimina CPFs invalidos conhecidos
-        if (cpf === "00000000000" ||
-          cpf === "11111111111" ||
-          cpf === "22222222222" ||
-          cpf === "33333333333" ||
-          cpf === "44444444444" ||
-          cpf === "55555555555" ||
-          cpf === "66666666666" ||
-          cpf === "77777777777" ||
-          cpf === "88888888888" ||
-          cpf === "99999999999")
-            this.isValidCPF = false;
-
-        // Valida 1o digito
-        for (let i=0; i < 9; i ++)
-          add += parseInt(cpf.charAt(i)) * (10 - i);
-          rev = 11 - (add % 11);
-          if (rev === 10 || rev === 11)
-            rev = 0;
-          if (rev != parseInt(cpf.charAt(9)))
-            this.isValidCPF = false;
-
-        // Valida 2o digito
-        add = 0;
-        for (let j = 0; j < 10; j ++)
-          add += parseInt(cpf.charAt(j)) * (11 - j);
-
-        rev = 11 - (add % 11);
-        if (rev === 10 || rev === 11)
-          rev = 0;
-
-        if (rev != parseInt(cpf.charAt(10)))
-          this.isValidCPF = false;
-      }
-    }
-    return this.isValidCPF;
-  }
-
-  validateCNPJ(cnpj) {
-    this.isValidCNPJ = true;
-
-    if(cnpj !== null) {
-      cnpj = cnpj.replace(/[^\d]+/g,'');
-
-      if (cnpj.length === 14) {
-        // Elimina CNPJs invalidos conhecidos
-        if (cnpj == "00000000000000" ||
-            cnpj == "11111111111111" ||
-            cnpj == "22222222222222" ||
-            cnpj == "33333333333333" ||
-            cnpj == "44444444444444" ||
-            cnpj == "55555555555555" ||
-            cnpj == "66666666666666" ||
-            cnpj == "77777777777777" ||
-            cnpj == "88888888888888" ||
-            cnpj == "99999999999999")
-              this.isValidCNPJ = false;
-
-        // Valida DVs
-        let tamanho = cnpj.length - 2
-        let numeros = cnpj.substring(0,tamanho);
-        let digitos = cnpj.substring(tamanho);
-        let soma = 0;
-        let pos = tamanho - 7;
-
-        for (let i = tamanho; i >= 1; i--) {
-          soma += numeros.charAt(tamanho - i) * pos--;
-          if (pos < 2)
-                pos = 9;
-        }
-
-        let resultado = soma % 11 < 2 ? 0 : 11 - soma % 11;
-        if (resultado != digitos.charAt(0))
-          this.isValidCNPJ = false;
-
-        tamanho = tamanho + 1;
-        numeros = cnpj.substring(0,tamanho);
-        soma = 0;
-        pos = tamanho - 7;
-
-        for (let j = tamanho; j >= 1; j--) {
-          soma += numeros.charAt(tamanho - j) * pos--;
-          if (pos < 2)
-                pos = 9;
-        }
-        resultado = soma % 11 < 2 ? 0 : 11 - soma % 11;
-        if (resultado != digitos.charAt(1))
-          this.isValidCNPJ = false;
-      }
-    }
-
-    return this.isValidCNPJ;
-  }
-
   search() {
-    var result = this.loginService.authenticate(this.login);
+    let result = this.loginService.authenticate(this.login);
     result.subscribe(data => this.router.navigate(["/"]));
   }
 
   reset() {
     this.login.cpf = "";
     this.login.cnpj = "";
+  }
+
+  // @Copyright to http://www.devmedia.com.br/validar-cpf-com-javascript/23916
+  validateCPF(strCPF) {
+    this.isValidCPF = true;
+
+    // Do not pass through the function if CPF number is equal to null
+    if (strCPF !== null) {
+      // Removing everything which is not number
+      strCPF = strCPF.replace(/[^\d]+/g,'');
+      // Validating just if user has typed all 11 numbers
+      if (strCPF.length === 11) {
+        let sum = 0;
+        let remainder = 0;
+
+        if (strCPF == "00000000000") {
+          this.isValidCPF = false;
+        }
+
+        for (let i = 1; i <= 9; i++) {
+          sum = sum + parseInt(strCPF.substring(i-1, i)) * (11 - i);
+        }
+
+        remainder = (sum * 10) % 11;
+        if ((remainder == 10) || (remainder == 11)) {
+          remainder = 0;
+        }
+        if (remainder != parseInt(strCPF.substring(9, 10))) {
+          this.isValidCPF = false;
+        }
+
+        sum = 0;
+        for (let j = 1; j <= 10; j++) {
+          sum = sum + parseInt(strCPF.substring(j-1, j)) * (12 - j);
+        }
+
+        remainder = (sum * 10) % 11;
+        if ((remainder == 10) || (remainder == 11)) {
+          remainder = 0;
+        }
+        if (remainder != parseInt(strCPF.substring(10, 11))) {
+          this.isValidCPF = false;
+        }
+      }
+    }
+    return this.isValidCPF;
+  }
+
+  // @Copyright to http://www.geradorcnpj.com/javascript-validar-cnpj.htm
+  validateCNPJ(strCNPJ) {
+    this.isValidCNPJ = true;
+
+    // Do not pass through the function if CNPJ number is equal to null
+    if(strCNPJ !== null) {
+      // Removing everything which is not number
+      strCNPJ = strCNPJ.replace(/[^\d]+/g,'');
+      // Validating just if user has typed all 14 numbers
+      if (strCNPJ.length === 14) {
+
+        if (strCNPJ == "00000000000000") {
+          this.isValidCNPJ = false;
+        }
+
+        let length = strCNPJ.length - 2
+        let numbers = strCNPJ.substring(0,length);
+        let digits = strCNPJ.substring(length);
+        let sum = 0;
+        let position = length - 7;
+
+        for (let i = length; i >= 1; i--) {
+          sum += numbers.charAt(length - i) * position--;
+          if (position < 2) {
+            position = 9;
+          }
+        }
+
+        let result = sum % 11 < 2 ? 0 : 11 - sum % 11;
+        if (result != digits.charAt(0)) {
+          this.isValidCNPJ = false;
+        }
+
+        length = length + 1;
+        numbers = strCNPJ.substring(0,length);
+        sum = 0;
+        position = length - 7;
+
+        for (let j = length; j >= 1; j--) {
+          sum += numbers.charAt(length - j) * position--;
+          if (position < 2) {
+            position = 9;
+          }
+        }
+
+        result = sum % 11 < 2 ? 0 : 11 - sum % 11;
+        if (result != digits.charAt(1)){
+          this.isValidCNPJ = false;
+        }
+      }
+    }
+
+    return this.isValidCNPJ;
   }
 }
